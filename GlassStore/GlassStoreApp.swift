fe407f53bf010c340c38506f3ca106d427b7867b
Robertto Flores <roberttozappiain@gmail.com>
2021-11-23 07:12:35 -0800

    Initial Commit

---
+++ GlassStore/GlassStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassStoreApp.swift
+//  GlassStore
+//
+//  Created by Robertto Flores on 23/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GlassStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
